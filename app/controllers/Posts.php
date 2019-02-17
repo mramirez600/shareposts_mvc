7ad708f82aab2e62ad68f1817478c27934e346b0
Marvin Ramirez <eldo@hotmail.com>
2019-02-17 00:48:30 -0500

    delete post commit

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -133,4 +133,25 @@ class Posts extends Controller {
 
     $this->view('posts/show', $data);
   }
+
+  public function delete($id){
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+      $post = $this->postModel->getPostById($id);
+      
+      // Check for owner
+      if($post->user_id != $_SESSION['user_id']){
+        redirect('posts');
+      }
+
+      if($this->postModel->deletePost($id)){
+        flash('post_message', 'Post removed.');
+        redirect('posts');
+      } else {
+        die('Something went wrong.');
+      }
+    } else {
+      redirect('posts');
+    }
+  }
 }
